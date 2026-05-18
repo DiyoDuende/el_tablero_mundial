84886032f9c91c2129a7da75c3481de1dcbd9437
DiyoDuende <elgrandiyo@gmail.com>
2026-05-18 14:52:41 +0200

    Update 00-mapa.js

--- js/ui/00-mapa.js
+++ js/ui/00-mapa.js
@@ -1,17 +1,22 @@
-// js/ui/00-mapa.js - Mapa con datos reales del Banco Mundial, Eurostat y OMS
+// js/ui/00-mapa.js - Mapa con datos reales del Banco Mundial
 const MapaMundial = {
   map: null,
   datosPaises: {},
 
   init: async function() {
+    // Inicializar el mapa
     this.map = L.map('mapa-mundial').setView([20, 0], 2);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap'
     }).addTo(this.map);
 
+    // Cargar datos económicos y luego el mapa
     await this.cargarTodosLosDatos();
+    await this.cargarMapa();
+  },
 
-    // Cargar GeoJSON de países (asegúrate de tener el archivo en esa ruta)
+  async cargarMapa() {
+    // Cargar GeoJSON de países desde una URL pública estable
     const response = await fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson');
     const geojson = await response.json();
     
@@ -24,7 +29,7 @@ const MapaMundial = {
   async cargarTodosLosDatos() {
     // Indicadores del Banco Mundial
     const indicadores = {
-      pib: 'NY.GDP.PCAP.PP.CD',
+      pib: 'NY.GDP.PCAP.PP.CD',      // PIB per cápita (PPA)
       poblacion: 'SP.POP.TOTL',
       desempleo: 'SL.UEM.TOTL.ZS',
       deuda: 'GC.DOD.TOTL.GD.ZS',
@@ -32,8 +37,8 @@ const MapaMundial = {
       energia: 'EG.USE.PCAP.KG.OE'
     };
 
-    // Obtener lista de códigos ISO3 desde el GeoJSON
-    const geojsonResp = await fetch('js/data/countries.geojson');
+    // Obtener lista de códigos ISO3 desde el GeoJSON remoto
+    const geojsonResp = await fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson');
     const geojson = await geojsonResp.json();
     const paises = geojson.features.map(f => f.properties.ISO_A3).filter(c => c && c !== '-99');
 
@@ -55,38 +60,6 @@ const MapaMundial = {
         }
       }
       await new Promise(r => setTimeout(r, 50));
-    }
-
-    // Datos de Eurostat (paro juvenil) para países europeos
-    await this.cargarEurostat();
-    // Datos de salud OMS (esperanza de vida)
-    await this.cargarOMS();
-  },
-
-  async cargarEurostat() {
-    const paisesEU = ['ESP', 'FRA', 'DEU', 'ITA', 'PRT', 'POL', 'SWE', 'NLD', 'BEL', 'AUT'];
-    for (let code of paisesEU) {
-      const url = `https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/une_rt_a/?format=JSON&geo=${code}&age=Y15-24&sex=T&unit=PC_ACT`;
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-        const values = Object.values(data.value);
-        const latest = values[values.length - 1];
-        if (latest && this.datosPaises[code]) this.datosPaises[code].paroJuvenil = latest;
-      } catch(e) {}
-    }
-  },
-
-  async cargarOMS() {
-    const paises = Object.keys(this.datosPaises);
-    for (let code of paises) {
-      const url = `https://ghoapi.azureedge.net/api/WHOSIS_000001?$filter=CountryCode eq '${code}'`;
-      try {
-        const res = await fetch(url);
-        const data = await res.json();
-        const esperanza = data.value?.[0]?.NumericValue;
-        if (esperanza && this.datosPaises[code]) this.datosPaises[code].esperanzaVida = esperanza;
-      } catch(e) {}
     }
   },
 
@@ -119,8 +92,6 @@ const MapaMundial = {
     const deuda = datos.deuda ? `${datos.deuda}%` : 'No disponible';
     const co2 = datos.co2 ? `${datos.co2} t` : 'No disponible';
     const energia = datos.energia ? `${datos.energia.toLocaleString()} kg` : 'No disponible';
-    const paroJuvenil = datos.paroJuvenil ? `${datos.paroJuvenil}%` : 'No disponible';
-    const esperanza = datos.esperanzaVida ? `${datos.esperanzaVida} años` : 'No disponible';
     
     layer.bindTooltip(feature.properties?.NAME || code);
     layer.bindPopup(`
@@ -131,9 +102,7 @@ const MapaMundial = {
       💸 Deuda pública (% PIB): ${deuda}<br>
       🌍 CO₂ per cápita: ${co2}<br>
       🔋 Energía per cápita: ${energia}<br>
-      🧑‍🎓 Paro juvenil (Eurostat): ${paroJuvenil}<br>
-      ❤️ Esperanza de vida: ${esperanza}<br>
-      <em>Fuentes: Banco Mundial, Eurostat, OMS</em>
+      <em>Datos: Banco Mundial (último año disponible)</em>
     `);
   }
 };
