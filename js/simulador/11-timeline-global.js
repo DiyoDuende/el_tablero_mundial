3fb5ed91ae537fbae8c4d0a2fe5a41f8fa9bc16b
DiyoDuende <elgrandiyo@gmail.com>
2026-05-20 16:44:12 +0200

    Add Buscador object for searching data by name

--- js/simulador/11-timeline-global.js
+++ js/simulador/11-timeline-global.js
@@ -58,3 +58,17 @@ const TimelineGlobal = {
 };
 
 window.TimelineGlobal = TimelineGlobal;
+
+const Buscador = {
+    buscar(texto, datos) {
+        const resultados = [];
+        const textoLower = texto.toLowerCase();
+        for (const id in datos) {
+            if (datos[id].nombre && datos[id].nombre.toLowerCase().includes(textoLower)) {
+                resultados.push({ id, ...datos[id] });
+            }
+        }
+        return resultados;
+    }
+};
+window.Buscador = Buscador;
