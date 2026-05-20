cf796903b13f81f3170b3e8a9198056b3ebb8e01
DiyoDuende <elgrandiyo@gmail.com>
2026-05-20 16:40:36 +0200

    Add Buscador object with search functionality

--- js/core/07-buscador.js
+++ js/core/07-buscador.js
@@ -1,1 +1,13 @@
-
+const Buscador = {
+    buscar(texto, datos) {
+        const resultados = [];
+        const textoLower = texto.toLowerCase();
+        for (const id in datos) {
+            if (datos[id].nombre && datos[id].nombre.toLowerCase().includes(textoLower)) {
+                resultados.push({ id, ...datos[id] });
+            }
+        }
+        return resultados;
+    }
+};
+window.Buscador = Buscador;
