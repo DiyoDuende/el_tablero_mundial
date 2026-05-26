fb61333de6a4b567928b1f5974867241b497548c
DiyoDuende <elgrandiyo@gmail.com>
2026-05-26 17:42:49 +0200

    Update 07-motor.js

--- js/simulador/07-motor.js
+++ js/simulador/07-motor.js
@@ -1,10 +1,11 @@
+// js/simulador/07-motor.js
 const MotorSimulacion = {
     simular: function(params) {
         const { poder = 0.5, sector = 0.5, mecanismo = 0.5, factorTerritorio = 1, factorEscala = 100 } = params;
         const base = poder * sector * mecanismo * factorTerritorio;
         return {
             impacto: {
-                económico: Math.round(base * factorEscala),
+                económico: Math.round(base * factorEscala * 1.0),
                 geopolítico: Math.round(base * factorEscala * 0.8),
                 social: Math.round(base * factorEscala * 0.5)
             }
